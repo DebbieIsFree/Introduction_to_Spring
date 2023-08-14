4e09a9d693164c755515881cf479ff994429e4de
Jiwon <wldnjs980409@naver.com>
2023-08-14 11:19:45 +0900

    welcome page (View 환경 설정)

--- src/main/java/hello/hellospring/HelloSpringApplication.java
+++ src/main/java/hello/hellospring/HelloSpringApplication.java
@@ -3,7 +3,7 @@ package hello.hellospring;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
+@SpringBootApplication		// tomcat 웹	서버 내장
 public class HelloSpringApplication {
 
 	public static void main(String[] args) {
